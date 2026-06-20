3581ea6f60281748949638aa1ce76c3ff57cf178
keisuke1974-sketch <keisuke1974@gmail.com>
2026-06-20 17:05:23 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'patchtool-pwa-v5-4';
+const CACHE_NAME = 'patchtool-pwa-v5-5';
 const ASSETS = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', event => {
